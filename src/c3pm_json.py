07d944caa4fed23f178aa782228192e0115c039a
Oleh Kurachenko <oleh.kurachenko@gmail.com>
2018-05-07 13:59:08 +0300

    C3PMJSON now can be consturcted directory from file

--- src/c3pm_json.py
+++ src/c3pm_json.py
@@ -15,10 +15,11 @@ import json
 class C3PMJSON:
     """
     Represents a c3pm.json file, which stores data about project and
-    it's dependencies. Class is being constructed from stirng representation
-    of c3pm.json containment. If no arguments given, a new C3PMJSON is being
-    constructed from users data via CLI. Class allows to make changes
-    and export new version of this file as string.
+    it's dependencies. Class is being constructed from string representation
+    of c3pm.json containment, or from a file directly. If no arguments given,
+    a new C3PMJSON is being constructed from users data via CLI. Class allows
+    to make changes  and export new version of this file as string or write it
+    directly to file.
 
     property "version" is a version of c3pm.json standard
     property "whatIsC3pm" is a url to c3pm project, which is added to all c3pm.json
@@ -30,21 +31,36 @@ class C3PMJSON:
 
     C3PM_JSON_FILENAME = "c3pm.json"
 
-    def __init__(self, json_str: str = None):
+    def __init__(self, json_str: str = None, load_from_file: bool = False):
         """
-        json_str is being parsed to get it's representation
+        If json_str is not None (default), it is being parsed to get it's representation. In this
+        case, load_from_file have to be False (default), otherwise it causes an assertion error.
+        If json_str is None and load_from_file if False, it is being constructed with users data
+        via CLI.
+        If json_str in None and load_from_file if True, containment is being red from file
+        self.C3PM_JSON_FILENAME.
         :param json_str: str - containment of c3pm.json
+        :param load_from_file: if True, json is being red directly from file
 
         :raises json.decoder.JSONDecodeError: if json_str is not a valid json
+        :raises FileNotFoundError: if self.C3PM_JSON_FILENAME cannot be opened
+        :raises AssertionError: if call parameters are forbidden
         """
+
+        assert not (json_str and load_from_file), "if json_str is not None (default)," \
+                                                  "load_from_file have to be False (default)"
 
         if json_str is not None:
             assert type(json_str) == str, "json_str type must be str"
 
             self.__c3pm_dict = json.loads(json_str, object_pairs_hook=OrderedDict)
         else:
-            self.__c3pm_dict = OrderedDict()
-            self.init_new_json()
+            if load_from_file:
+                with open(self.C3PM_JSON_FILENAME, "r") as c3pm_json_file:
+                    self.__c3pm_dict = json.loads(c3pm_json_file.read(), object_pairs_hook=OrderedDict)
+            else:
+                self.__c3pm_dict = OrderedDict()
+                self.init_new_json()
 
     def init_new_json(self):
         """
